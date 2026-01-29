2479b4cbfa13ef198fb1029eaa00900b92886ec6
dhirajdhande19 <dhirajdhande2@gmail.com>
2026-01-29 13:48:52 +0530

    fix: invalid service urls

--- gateway/src/modules/orders/orders.controller.js
+++ gateway/src/modules/orders/orders.controller.js
@@ -6,12 +6,18 @@ export const forwardToOrdersService = async (req, res) => {
     const response = await axios({
       method: req.method,
       url: `${ORDERS_BASE_URL}${req.originalUrl}`,
-      headers: req.headers,
+      headers: { ...req.headers, host: undefined },
+      validateStatus: () => true,
     });
 
     return res.status(response.status).json(response.data);
   } catch (e) {
     console.error({ message: e.message });
+
+    if (e.response) {
+      return res.status(e.response.status).json(e.response.data);
+    }
+
     return res.status(503).json({ message: "Bad Gateway" });
   }
 };
--- gateway/src/modules/users/users.controller.js
+++ gateway/src/modules/users/users.controller.js
@@ -5,13 +5,19 @@ export const forwardToUserService = async (req, res) => {
   try {
     const response = await axios({
       method: req.method,
-      headers: req.headers,
       url: `${USERS_BASE_URL}${req.originalUrl}`,
+      headers: { ...req.headers, host: undefined },
+      validateStatus: () => true,
     });
 
     return res.status(response.status).json(response.data);
   } catch (e) {
     console.error({ message: e.message });
+
+    if (e.response) {
+      return res.status(e.response.status).json(e.response.data);
+    }
+
     return res.status(502).json({ message: "Bad Gateway" });
   }
 };
